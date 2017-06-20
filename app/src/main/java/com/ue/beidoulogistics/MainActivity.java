d3409d7a9a786df998115f44b7c1bb120ae91d41
mashang123456 <865909475@qq.com>
2017-06-20 09:49:46 +0800

    Initial commit

---
+++ app/src/main/java/com/ue/beidoulogistics/MainActivity.java
@@ -0,0 +1,39 @@
+package com.ue.beidoulogistics;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.baidu.mapapi.map.MapView;
+
+public class MainActivity extends Activity {
+    MapView mMapView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //获取地图控件引用，默认显示天安门
+        mMapView = (MapView) findViewById(R.id.bmapView);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+
+}
